242087191b974b00cf7caf8ea51f0fe73e4a2133
Sandesh <sandesh41.work@gmail.com>
2026-06-01 15:48:38 +0530

    implemented navigation based on user login state

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,16 +2,39 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../features/auth/LoginScreen';
 import HomeScreen from '../features/feed/HomeScreen';
-
+import { User } from 'firebase/auth';
 import { RootStackParamList } from '../shared/types/navigation';
+import { useEffect, useState } from 'react';
+import { auth } from '../shared/services/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { ActivityIndicator } from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
 function RootNavigator() {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser)
+            setLoading(false)
+        })
+        return unsubscribe // cleanup
+    }, [])
+
+    if (loading) return <ActivityIndicator />
+
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
+                {
+                    user ? (
+                        <Stack.Screen name="Home" component={HomeScreen} />
+                    ) : (
+                        <Stack.Screen name="Login" component={LoginScreen} />
+                    )
+                }
             </Stack.Navigator>
         </NavigationContainer>
     )
