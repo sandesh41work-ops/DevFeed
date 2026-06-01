bf903897738a3c53ae6b841eda8877a9b3f721d9
Sandesh <sandesh41.work@gmail.com>
2026-06-01 16:30:31 +0530

    implemented logout button in header

--- src/features/feed/HomeScreen.tsx
+++ src/features/feed/HomeScreen.tsx
@@ -1,17 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
 import { getStory, getTopStories } from '../../shared/services/api';
 import { Story } from '../../shared/types/story';
 import StoryCard from '../../shared/components/StoryCard';
 import Button from '../../shared/components/Button';
+import { useNavigation } from '@react-navigation/native';
+import { logOutUser } from '../auth/authService';
+
 const HomeScreen = () => {
   const [loading, setLoading] = useState(true);
   const [stories, setStories] = useState<Story[]>([]);
   const [error, setError] = useState("");
 
+  const navigation = useNavigation();
+
   useEffect(() => {
     fetchStories()
   }, [])
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button title="Logout" onPress={logOutUser} style={styles.logoutBtn}/>
+      )
+    })
+  }, [navigation])
+
   const fetchStories = async () => {
     try {
       setLoading(true);
@@ -48,3 +62,10 @@ const HomeScreen = () => {
 
 export default HomeScreen
 
+const styles = StyleSheet.create({
+  logoutBtn: {
+    backgroundColor: '#ef4444',
+    paddingHorizontal: 12,
+    height: 36,
+  }
+})  
