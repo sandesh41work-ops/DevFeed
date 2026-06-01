c70e2744512d18fd96d3df8bb2ddc75f7c9fdffa
Sandesh <sandesh41.work@gmail.com>
2026-06-01 14:42:09 +0530

    implemented homescreen for fetching top stories and displaying stories

--- src/features/feed/HomeScreen.tsx
+++ src/features/feed/HomeScreen.tsx
@@ -1,10 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+import { getStory, getTopStories } from '../../shared/services/api';
+import { Story } from '../../shared/types/story';
+import StoryCard from '../../shared/components/StoryCard';
 
 const HomeScreen = () => {
+  const [loading, setLoading] = useState(true);
+  const [stories, setStories] = useState<Story[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchStories()
+  }, [])
+  const fetchStories = async () => {
+    try {
+      setLoading(true);
+      const ids = await getTopStories();
+      const storyPromises = ids.map(id => getStory(id))
+      const data = await Promise.all(storyPromises);
+      setStories(data);
+
+    } catch (error) {
+      setError("Failed to load stories");
+    } finally {
+      setLoading(false);
+    }
+  }
+  if (loading) return <ActivityIndicator />
+  if (error) return <Text>{error}</Text>
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <FlatList
+        data={stories}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <StoryCard story={item} />}
+      />
     </View>
   )
 }
