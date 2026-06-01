9689816fa9f714af86b7dbe9aab6babbb5892524
Sandesh <sandesh41.work@gmail.com>
2026-06-01 19:02:39 +0530

    added register user method

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -1,10 +1,17 @@
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../../shared/services/firebase"
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 export const loginUser = async (email: string, password: string) => {
   const result = await signInWithEmailAndPassword(auth, email, password)
   return result.user
 }
+
 export const logOutUser = async () => {
   await auth.signOut();
+}
+
+export const registerUser = async (email: string, password: string) => {
+  const result = await createUserWithEmailAndPassword(auth, email, password)
+  return result.user
 }
