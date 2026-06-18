e509288b3161078e8b0cb283fd005c045c38bafb
Sandesh <sandesh41.work@gmail.com>
2026-06-18 15:00:26 +0530

    Extracted StoryDetail Card and Use Again in ArticleDetailsScreen

--- src/features/feed/ArticleDetailScreen.tsx
+++ src/features/feed/ArticleDetailScreen.tsx
@@ -3,119 +3,19 @@ import { useRoute } from "@react-navigation/native";
 import Button from "../../shared/components/Button";
 import { Story } from "../../shared/types/story";
 import { useTheme } from "../../shared/hooks/useTheme";
-import { useEffect, useState } from "react";
-import {
-  addBookmark,
-  isBookmarked,
-  removeBookmark,
-} from "../../shared/services/bookmarkService";
-import Discussion from "../discussion/Discussion";
+import StoryDetailsCard from "../../shared/components/StoryDetailsCard";
 const ArticleDetailScreen = () => {
-  const [bookmarked, setBookmarked] = useState(false);
   const route = useRoute<any>();
   const { story }: { story: Story } = route.params;
-
-  const publishedDate = new Date(story.time * 1000);
   const { colors } = useTheme();
-  const openURL = () => {
-    if (story.url) {
-      Linking.openURL(story.url);
-    }
-  };
-
-  useEffect(() => {
-    isBookmarked(story.id).then(setBookmarked);
-  }, []);
-
-
-  const toggleBookmark = async () => {
-    if (bookmarked) {
-      await removeBookmark(story.id);
-    } else {
-      await addBookmark(story);
-    }
-    setBookmarked(!bookmarked);
-  };
   return (
     <ScrollView>
 
       <View style={[styles.container, { backgroundColor: colors.background }]}>
-        <View
-          style={[
-            styles.card,
-            { backgroundColor: colors.card },
-            {
-              backgroundColor: colors.card,
-              borderWidth: 1,
-              borderColor: colors.border,
-            },
-          ]}
-        >
-          <Text style={[styles.title, { color: colors.text }]}>
-            {story.title}
-          </Text>
+        <StoryDetailsCard story={story} />
 
-          <Text style={[styles.author, { color: colors.subtext }]}>
-            By {story.by}
-          </Text>
-
-          <Text style={[styles.date, { color: colors.subtext }]}>
-            {publishedDate.toLocaleString()}
-          </Text>
-
-          <View style={styles.statsRow}>
-            <View style={[styles.statCard, { backgroundColor: colors.border }]}>
-              <Text style={[styles.statValue, { color: colors.accent }]}>
-                {story.score}
-              </Text>
-              <Text style={[styles.statLabel, { color: colors.subtext }]}>
-                Points
-              </Text>
-            </View>
-
-            <View style={[styles.statCard, { backgroundColor: colors.border }]}>
-              <Text style={[styles.statValue, { color: colors.accent }]}>
-                {story.descendants ?? 0}
-              </Text>
-              <Text style={[styles.statLabel, { color: colors.subtext }]}>
-                Comments
-              </Text>
-            </View>
-          </View>
-
-          <View style={styles.typeContainer}>
-            <Text style={[styles.typeText, { color: colors.text }]}>
-              Type: {story.type}
-            </Text>
-          </View>
-
-          <View style={styles.buttonRow}>
-            {story.url && (
-              <Button
-                style={styles.actionButton}
-                title="Read Article"
-                onPress={openURL}
-              />
-            )}
-
-            <Button
-              style={[{
-                padding: 10,
-                margin: 10,
-                width: "auto",
-                backgroundColor: bookmarked ? "#EF4444" : colors.accent,
-              }, styles.actionButton]}
-              title={bookmarked ? "Remove" : "Bookmark"}
-              onPress={toggleBookmark}
-            />
-          </View>
-        </View>
-
-        <Discussion>
-
-        </Discussion>
       </View>
-      
+
     </ScrollView>
 
   );
