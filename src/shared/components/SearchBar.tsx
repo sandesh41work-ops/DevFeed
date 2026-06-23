cfe8f5b5a335e2fd26c5facabe9285a7389f3d9d
Sandesh <sandesh41.work@gmail.com>
2026-06-23 16:19:05 +0530

    added clear icon

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,42 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "../hooks/useTheme";
 import Input from "./Input";
 type SearchBarProps = {
-    value: string;
-    onChangeText: (text: string) => void;
-    placeholder?: string;
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder?: string;
 };
 
 const SearchBar = ({
-    value,
-    onChangeText,
-    placeholder = "Search stories...",
+  value,
+  onChangeText,
+  placeholder = "Search stories...",
 }: SearchBarProps) => {
-    const {colors} = useTheme();
-    return (
-        <View style={[styles.container, {backgroundColor: colors.background}]}>
-            <Input
-                value={value}
-                onChangeText={onChangeText}
-                placeholder={placeholder}
-                customStyles = {{marginBottom : 1}}
-            />
-            {/* Todo : Clear Icon */}
-            <TouchableOpacity onPress={() => onChangeText("")} style={{ position: "absolute", right: 40, top: 15, padding: 4 }}>
-                <Text style={{ fontSize: 20, color: "#6d6262" }}>X</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  const [clearButtonPressed, setClearButtonPressed] = useState(false);
+  const { colors } = useTheme();
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <Input
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        customStyles={{ marginBottom: 1 }}
+      />
+      {/* Todo : Clear Icon */}
+      <TouchableOpacity
+        onPress={() => onChangeText("")}
+        onPressIn={() => setClearButtonPressed(true)}
+        onPressOut={() => setClearButtonPressed(false)}
+        style={{ position: "absolute", right: 30, top: 13, padding: 4 }}
+      >
+        <Ionicons
+          name="close-circle"
+          size={32}
+          color={clearButtonPressed ? "#e04646" : "#6d6262"}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default React.memo(SearchBar);
 
 const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 12,
-        paddingVertical: 10,
-        // borderBottomWidth: 1,
-        // borderColor: "#47474783",
-    },
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    // borderBottomWidth: 1,
+    // borderColor: "#47474783",
+  },
 });
