681daca4143c966ae3ac68f2718e7444855eacf4
Sandesh <sandesh41.work@gmail.com>
2026-08-31 17:14:19 +0530

    implemeented basic layout

--- src/features/profile/ProfileScreen.tsx
+++ src/features/profile/ProfileScreen.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, View, Platform } from "react-native";
+import { useEffect } from "react";
+import { useObserve } from "expo-observe";
+import { useTheme } from "../../shared/hooks/useTheme";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import AppHeader from "../../shared/components/AppHeader";
 
 const ProfileScreen = () => {
+  const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
+  const { markInteractive } = useObserve();
+
+  useEffect(() => {
+    markInteractive();
+  }, [markInteractive]);
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        <AppHeader />
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={[
+            styles.content,
+            { paddingBottom: insets.bottom + 24 },
+          ]}
+          showsVerticalScrollIndicator={false}
+        >
+          {/* Profile content will be added here */}
 
-export default ProfileScreen
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  content: {
+    padding: 24,
+    paddingBottom: 40,
+  },
+});
